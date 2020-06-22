55bf9b64deb9a34a0a8aaee964ed92ac6692c243
Guilherme Girardi <girardi.gui@icloud.com>
2020-06-22 13:11:01 +0100

    Create index.js for all pages and components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { IconContext } from "react-icons";
+import { AiFillFacebook } from 'react-icons/ai';
+import { AiFillInstagram } from 'react-icons/ai';
+import { AiFillYoutube } from 'react-icons/ai';
+import { FaPinterestSquare } from 'react-icons/fa';
+
+import {
+  Container,
+  SocialMedia,
+  FooterLinks,
+  Routes
+} from './styles';
+
+class Footer extends Component {
+    render () {
+      return (
+        <Container>
+          <SocialMedia>
+          <IconContext.Provider value={{ color: "#474547", size: "3rem"  }}>
+            <AiFillFacebook className="icons" />
+            <AiFillInstagram className="icons" />
+            <AiFillYoutube className="icons" />
+            <FaPinterestSquare className="icons" />
+          </IconContext.Provider>
+        </SocialMedia>
+
+        <FooterLinks>
+          <div>
+            <h4>Explore</h4>
+            <Routes to="/shop">Shop</Routes>
+            <Routes to="/about" >About</Routes>
+            <Routes to="/contact" >Contact</Routes>
+          </div>
+
+          <div>
+            <h4>Join the tribe</h4>
+            <p>Sign up for email updates</p>
+            <input type="text"/>
+            <button type="button">Join</button>
+          </div>
+        </FooterLinks>
+        </Container>
+      )
+    }
+};
+
+export default Footer;
